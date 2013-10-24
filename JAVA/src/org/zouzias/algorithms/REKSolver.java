47927860fd59f57a591f89c77d10fb71e7a9296d
zouzias <zouzias@gmail.com>
2013-10-24 10:02:08 +0200

    Minor changes

--- JAVA/src/org/zouzias/algorithms/REKSolver.java
+++ JAVA/src/org/zouzias/algorithms/REKSolver.java
@@ -33,19 +33,19 @@ public class REKSolver implements LeastSquaresSolver {
         int i_k, j_k, k = 0;
         DoubleVector x = new DenseVector(A.getColumnDimension());
         DoubleVector z = new DenseVector(b);
-        DoubleVector rowProbs = A.rowProbs(), colProbs = A.columnProbs();
-        AliasMethod rowSampler = new AliasMethod(rowProbs.asList());
-        AliasMethod colSampler = new AliasMethod(colProbs.asList());
+        DoubleVector rowNorms = A.rowNorms(), colNorms = A.columnNorms();
+        AliasMethod rowSampler = new AliasMethod(rowNorms.asList());
+        AliasMethod colSampler = new AliasMethod(colNorms.asList());
 
         while ( k % CheckEvery != 0 || !timeIsUp(initTime, maxSeconds) ) {
             i_k = rowSampler.next();
             j_k = colSampler.next();
 
-            val = -z.DDOT(A.getColumn(j_k)) / colProbs.get(j_k);       // val = - dot(z, A(:, j_k)) / colProbs(j_k) 
+            val = -z.DDOT(A.getColumn(j_k)) / colNorms.get(j_k);        // val = - dot(z, A(:, j_k)) / colProbs(j_k) 
             z.DAXPY(val, A.getColumn(j_k));                             // z = z + val * A(:, j_k);
 
             val = x.DDOT(A.getRow(i_k));                                // val = dot(x, A(i_k, :))
-            val = (b.get(i_k) - z.get(i_k) - val) / rowProbs.get(i_k);  // val = (b(i_k) - z(i_k) - val) / roProbs(i_k)
+            val = (b.get(i_k) - z.get(i_k) - val) / rowNorms.get(i_k);  // val = (b(i_k) - z(i_k) - val) / roProbs(i_k)
             x.DAXPY(val, A.getRow(i_k));                                // x = x + val * A(i_k, :);            
             
             k++;
@@ -68,20 +68,20 @@ public class REKSolver implements LeastSquaresSolver {
         int i_k, j_k;
         DoubleVector x = new DenseVector(A.getColumnDimension());
         DoubleVector z = new DenseVector(b);
-        DoubleVector rowProbs = A.rowProbs(), colProbs = A.columnProbs();
+        DoubleVector rowNorms = A.rowNorms(), columnNorms = A.columnNorms();
 
-        AliasMethod rowSampler = new AliasMethod(rowProbs.asList());
-        AliasMethod colSampler = new AliasMethod(colProbs.asList());
+        AliasMethod rowSampler = new AliasMethod(rowNorms.asList());
+        AliasMethod colSampler = new AliasMethod(columnNorms.asList());
 
         for (int k = 0; k < MaxIterations; k++) {
             i_k = rowSampler.next();
             j_k = colSampler.next();
 
-            val = -z.DDOT(A.getColumn(j_k)) / colProbs.get(j_k);       // val = - dot(z, A(:, j_k)) / colProbs(j_k) 
+            val = -z.DDOT(A.getColumn(j_k)) / columnNorms.get(j_k);     // val = - dot(z, A(:, j_k)) / colProbs(j_k) 
             z.DAXPY(val, A.getColumn(j_k));                             // z = z + val * A(:, j_k);
 
             val = x.DDOT(A.getRow(i_k));                                // val = dot(x, A(i_k, :))
-            val = (b.get(i_k) - z.get(i_k) - val) / rowProbs.get(i_k);  // val = (b(i_k) - z(i_k) - val) / roProbs(i_k)
+            val = (b.get(i_k) - z.get(i_k) - val) / rowNorms.get(i_k);  // val = (b(i_k) - z(i_k) - val) / roProbs(i_k)
             x.DAXPY(val, A.getRow(i_k));                                // x = x + val * A(i_k, :);  
         }
 
