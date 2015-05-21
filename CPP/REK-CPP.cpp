8582451f0984ad257b4ea9dcfc10ef5127b3198d
Anastasios Zouzias <zouzias@gmail.com>
2015-05-21 16:57:27 +0200

    CPP: Simple example

--- CPP/REK-CPP.cpp
+++ CPP/REK-CPP.cpp
@@ -8,9 +8,71 @@
 
 #include <iostream>
 
+#include "samplers/AliasSampler.hpp"
+#include "algorithms/REKSolver.hpp"
+
+
+#include "matrix/impl/DenseMatrix.hpp"
+#include "vector/DenseVector.hpp"
+
 using namespace std;
 
 int main(void) {
+
+
+	int n = 10;
+	std::vector<double> test(n);
+
+	test[0] = n;
+	for(int i = 1; i <n ; i++){
+		test[i] = 1;
+	}
+
+	AliasSampler alias(test);
+
+	alias.initSampler();
+
+
+	std::vector<uint>* samples = alias.sample(30);
+
+	for(int i = 0 ; i < samples->size(); i++){
+		cout<<"Sample " << samples->at(i) << endl;
+	}
+
+	DoubleMatrix* A = new DenseMatrix(10,10);
+	DoubleVector* xopt = new DenseVector(10);
+	xopt->random();
+	A->random();
+	DoubleVector* b = A->times(*xopt);
+
+	cout<< "xopt is " <<endl;
+	for (int i = 0 ; i < xopt->size(); i++){
+		cout<< xopt->get(i) <<endl;
+	}
+
+	cout<< "B is " <<endl;
+	for (int i = 0 ; i < b->size(); i++){
+		cout<< b->get(i) <<endl;
+	}
+
+	cout<< "B is " <<endl;
+	for (int i = 0 ; i < A->getRowDimension(); i++){
+		for (int j = 0 ; j < A->getColumnDimension(); j++){
+		cout<< A->get(i,j)<< " , ";
+		}
+		cout<<endl;
+	}
+
+
+	REKSolver solver = REKSolver();
+
+	long ITERS = 1000;
+	DoubleVector* x = solver.solve(*A, *b, ITERS);
+
+
+	x->minus(*xopt);
+	cout<< "Error is " << x->DNRM2();
+
 
 	cout<<"Hello World" <<endl;
 	return EXIT_SUCCESS;
