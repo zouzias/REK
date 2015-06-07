297edfd97cbc65303b520d336eb70819157e89fb
Anastasios Zouzias <zouzias@gmail.com>
2015-06-07 11:03:12 +0200

    Moved back to extended Kaczmarz

--- CPP/algorithms/REKSolver.cpp
+++ CPP/algorithms/REKSolver.cpp
@@ -9,6 +9,7 @@
 
 #include "../vector/DenseVector.hpp"
 #include "../samplers/AliasSampler.hpp"
+#include <stdlib.h>
 
 
 DoubleVector*	 REKSolver::solve(const DoubleMatrix& A, const DoubleVector& b, double MaxSeconds){
@@ -30,9 +31,18 @@ DoubleVector* REKSolver::solve(const DoubleMatrix& A, const DoubleVector& b, lon
   AliasSampler rowSampler(*rowNorms);
 	AliasSampler colSampler(*columnNorms);
 
+	// Initialize Alias samplers, O(n)
+	rowSampler.initSampler();
+	colSampler.initSampler();
+
   for (int k = 0; k < MaxIterations; k++) {
-  	i_k = rowSampler.walkerSample();
-  	j_k = colSampler.walkerSample();
+  	//i_k = rowSampler.walkerSample();
+  	//j_k = colSampler.walkerSample();
+
+		// Extended Kaczmarz
+		i_k = k % A.getRowDimension();
+		j_k = k % A.getColumnDimension();
+
 
     val = -z->DDOT(A.getColumn(j_k)) / columnNorms->get(j_k);     // val = - dot(z, A(:, j_k)) / colProbs(j_k)
     z->DAXPY(val, A.getColumn(j_k));                             // z = z + val * A(:, j_k);
